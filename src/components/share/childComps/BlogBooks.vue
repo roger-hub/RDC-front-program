7d11b0176722fe90ec359679a4df0aed8a5baeea
roger-hub <58379214+roger-hub@users.noreply.github.com>
2020-11-04 19:32:45 +0800

    bug-fix

--- src/components/share/childComps/BlogBooks.vue
+++ src/components/share/childComps/BlogBooks.vue
@@ -1,170 +1,180 @@
 <template>
-  <div class="blog-books">
+<div class="blog-books">
     <div class="shr-header">
-      <div class="shr-title fl">
-        <span class="fontsize"></span>
-        <span class="shr-txt font18">
-          书籍推荐
-          <div class="shr-under under16"></div>
-        </span>
-      </div>
-      <div class="books-nav fr">
-        <div class="direction">
-          <div class="direction-now" @click="showAllDir">
-            {{ dir }}
-            <i class="el-icon-caret-bottom"></i>
-          </div>
+        <div class="shr-title fl">
+            <span class="fontsize"></span>
+            <span class="shr-txt font18">
+                书籍推荐
+                <div class="shr-under under16"></div>
+            </span>
         </div>
-        <ul v-show="isDirShow" @click="chooseDir">
-          <li
-            v-for="(item, index) in allDir"
-            :key="index"
-            v-show="item === dir ? false : true"
-          >{{ item }}</li>
-        </ul>
-      </div>
+        <div class="books-nav fr">
+            <div class="direction">
+                <div class="direction-now" @click="showAllDir">
+                    {{ dir }}
+                    <i class="el-icon-caret-bottom"></i>
+                </div>
+            </div>
+            <ul v-show="isDirShow" @click="chooseDir">
+                <li v-for="(item, index) in allDir" :key="index" v-show="item === dir ? false : true">{{ item }}</li>
+            </ul>
+        </div>
     </div>
     <div class="books-container">
-      <div class="books-bd" v-for="(item, index) in booksItem" :key="index">
-        <div class="books-pic">
-          <img :src="item.coverUrl" alt />
+        <div class="books-bd" v-for="(item, index) in booksItem" :key="index">
+            <div class="books-pic">
+                <img :src="item.coverUrl" alt />
+            </div>
+            <div class="books-ctr">
+                <div class="books-tit">{{ item.bookName }}</div>
+                <div class="books-author">{{ item.author }}</div>
+                <div class="books-content"></div>
+            </div>
         </div>
-        <div class="books-ctr">
-          <div class="books-tit">{{ item.bookName }}</div>
-          <div class="books-author">{{ item.author }}</div>
-          <div class="books-content"></div>
-        </div>
-      </div>
     </div>
     <div class="books-more fr" @click="getMoreBooks">
-      <i class="el-icon-refresh"></i>
-      刷新
+        <i class="el-icon-refresh"></i>
+        刷新
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  // 部件
-  components: {},
-  data() {
-    return {
-      booksItem: [],
-      isDirShow: false,
-      dir: "前端",
-      allDir: ["前端", "后台", "安卓", "大数据"],
-    };
-  },
-  watch: {},
-  methods: {
-    showAllDir() {
-      this.isDirShow = !this.isDirShow;
+    // 部件
+    components: {},
+    data() {
+        return {
+            booksItem: [],
+            isDirShow: false,
+            dir: "前端",
+            allDir: ["前端", "后台", "安卓", "大数据"],
+        };
     },
-    getMoreBooks() {
-      this.$axios
-        .post("/first/book/queryBooks_02", {
-          direction: this.dir,
-        })
-        .then((res) => {
-          this.booksItem = res.data;
-        });
+    watch: {},
+    methods: {
+        showAllDir() {
+            this.isDirShow = !this.isDirShow;
+        },
+        getMoreBooks() {
+            this.$axios
+                .post("/first/book/queryBooks_02", {
+                    direction: this.dir,
+                })
+                .then((res) => {
+                    this.booksItem = res.data;
+                });
+        },
+        chooseDir(event) {
+            this.dir = event.target.innerText;
+            this.$axios
+                .post("/first/book/queryBooks_02", {
+                    direction: this.dir,
+                })
+                .then((res) => {
+                    this.isDirShow = !this.isDirShow;
+                    this.booksItem = res.data;
+                });
+        },
     },
-    chooseDir(event) {
-      this.dir = event.target.innerText;
-      this.$axios
-        .post("/first/book/queryBooks_02", {
-          direction: this.dir,
-        })
-        .then((res) => {
-          this.isDirShow = !this.isDirShow;
-          this.booksItem = res.data;
-        });
+    created() {
+        this.$axios
+            .post("/first/book/queryBooks_02", {
+                direction: "前端",
+            })
+            .then((res) => {
+                this.booksItem = res.data;
+            });
     },
-  },
-  created() {
-    this.$axios
-      .post("/first/book/queryBooks_02", {
-        direction: "前端",
-      })
-      .then((res) => {
-        this.booksItem = res.data;
-      });
-  },
 };
 </script>
 
 <style scoped>
 .books-nav {
-  position: relative;
-  padding-right: 15px;
-  line-height: 40px;
-  font-size: 12px;
-  color: #333;
-  cursor: pointer;
+    position: relative;
+    padding-right: 15px;
+    line-height: 40px;
+    font-size: 12px;
+    color: #333;
+    cursor: pointer;
 }
+
 .books-nav ul {
-  position: absolute;
-  top: 100%;
-  right: 0;
-  width: 70px;
-  background-color: #fff;
-  box-shadow: 0px 3px 3px #ccc;
+    position: absolute;
+    top: 100%;
+    right: 0;
+    width: 70px;
+    background-color: #fff;
+    box-shadow: 0px 3px 3px #ccc;
 }
-.books-nav ul > li {
-  height: 30px;
-  line-height: 30px;
-  padding: 0 0 0 15px;
+
+.books-nav ul>li {
+    height: 30px;
+    line-height: 30px;
+    padding: 0 0 0 15px;
 }
-.books-nav ul > li:hover {
-  color: #0d6aa7;
+
+.books-nav ul>li:hover {
+    color: #0d6aa7;
 }
+
 .books-container {
-  min-height: 100px;
+    min-height: 100px;
 }
+
 .books-bd {
-  display: flex;
-  cursor: pointer;
-  border-bottom: 1px dotted #ccc;
-  transition: 0.5s;
+    display: flex;
+    cursor: pointer;
+    border-bottom: 1px dotted #ccc;
+    transition: 0.5s;
 }
+
 .books-bd:hover {
-  background-color: rgb(245, 245, 245);
-  box-shadow: 0 0 3px #e0e0e0;
+    background-color: rgb(245, 245, 245);
+    box-shadow: 0 0 3px #e0e0e0;
 }
+
 .books-pic {
-  display: flex;
-  width: 100px;
-  height: 150px;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    width: 100px;
+    height: 150px;
+    justify-content: center;
+    align-items: center;
 }
+
 .books-pic img {
-  width: 90px;
-  height: 95px;
+    width: 90px;
+    height: 95px;
 }
+
 .books-ctr {
-  flex: 1;
-  padding: 20px 5px;
+    flex: 1;
+    padding: 20px 5px;
 }
+
 .books-tit {
-  margin-bottom: 10px;
-  font-size: 16px;
-  color: #333;
+    margin-bottom: 10px;
+    font-size: 16px;
+    color: #333;
 }
+
 .books-tit:hover {
-  color: #0d6aa7;
+    color: #0d6aa7;
 }
+
 .books-author {
-  font-size: 12px;
-  color: #999;
+    font-size: 12px;
+    color: #999;
 }
+
 .books-more {
-  margin-top: 20px;
-  padding-right: 15px;
-  font-size: 12px;
-  cursor: pointer;
+    margin-top: 20px;
+    padding-right: 15px;
+    font-size: 12px;
+    cursor: pointer;
 }
+
 .books-more:hover {
-  color: #0d6aa7;
+    color: #0d6aa7;
 }
 </style>
--- src/components/share/childComps/TwoColumn.vue
+++ src/components/share/childComps/TwoColumn.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
+<div>
     <div id="blog-content">
-      <div class="container">
-        <div id="blog-l" class="fl">
-          <blog-content></blog-content>
+        <div class="container">
+            <div id="blog-l" class="fl">
+                <blog-content></blog-content>
+            </div>
+            <div id="blog-r" class="fr">
+                <blog-search></blog-search>
+                <blog-menu></blog-menu>
+                <blog-tags></blog-tags>
+                <blog-books></blog-books>
+            </div>
         </div>
-        <div id="blog-r" class="fr">
-          <blog-search></blog-search>
-          <blog-menu></blog-menu>
-          <blog-tags></blog-tags>
-          <blog-books></blog-books>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -24,24 +24,44 @@ import BlogTags from "@/components/share/childComps/BlogTags";
 import BlogBooks from "@/components/share/childComps/BlogBooks";
 import BlogSearch from "@/components/share/childComps/BlogSearch";
 export default {
-  name: "TwoColumn",
-  components: {
-    BlogCardAll,
-    BlogContent,
-    BlogSearch,
-    BlogMenu,
-    BlogTags,
-    BlogBooks,
-  },
+    name: "TwoColumn",
+    components: {
+        BlogCardAll,
+        BlogContent,
+        BlogSearch,
+        BlogMenu,
+        BlogTags,
+        BlogBooks,
+    },
 };
 </script>
 
 <style scoped>
 #blog-l {
-  width: 900px;
+    width: 900px;
 }
+
 #blog-r {
-  width: 280px;
-  padding: 15px 0;
+    width: 280px;
+    padding: 15px 0;
+}
+
+.container {
+    width: 1200px;
+    margin: 0 auto;
+}
+
+.container::before {
+    content: "";
+    display: block;
+    clear: both;
+}
+
+.fr {
+    float: right;
+}
+
+.fl {
+    float: left;
 }
 </style>
