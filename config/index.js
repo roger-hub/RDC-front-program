c951e4e296b5045af0a89072fc387bfdb855b026
roger-hub <58379214+roger-hub@users.noreply.github.com>
2020-11-06 14:36:27 +0800

    bug-fix

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsPublicPath: "./",
     proxyTable: {
       "/first": {
         target: "http://aniun.top",
